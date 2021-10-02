21c98ff292f35aba87d8eb28189ff22da11f2cde
Rui Zhi <gohruizhi@yahoo.com.sg>
2021-10-02 12:10:57 +0800

    Initialize structure for controller

--- services/editor/api/controller.js
+++ services/editor/api/controller.js
@@ -1,5 +1,19 @@
-/* Defines the logic used in handling requests */
-class EditorController {
+/* Controllers will editor services */
+const service = require("../services/service");
+class ConnectionController {
   constructor() {}
+  create(req, res) {
+    console.log("Calling the create service");
+    service.create();
+    res.send("Connection created");
+  }
+
+  delete(req, res) {
+    console.log("Calling the delete service");
+    service.delete();
+    res.send("Connection deleted");
+  }
 }
-module.exports = EditorController;
+
+let connectionController = new ConnectionController();
+module.exports = connectionController;
