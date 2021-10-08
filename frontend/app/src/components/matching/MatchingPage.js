629c1383ce356641def481989ce7cb45ffbd64ff
Ashleigh <ashleighaathar@gmail.com>
2021-10-08 21:40:34 +0800

    add different views for matching page

--- frontend/app/src/components/matching/MatchingPage.js
+++ frontend/app/src/components/matching/MatchingPage.js
@@ -1,16 +1,128 @@
 import React from "react";
 
-import { Breadcrumb, Layout } from "antd";
+import { Layout } from "antd";
+import SelectionView from "./SelectionView";
+import LoadingView from "./LoadingView";
+import TimeoutView from "./TimeoutView";
 
 const { Content } = Layout;
 
+
+const views = {
+  loading: "LOADING",
+  selection: "SELECTION",
+  timeout: "TIMEOUT",
+};
+
+const questionDifficulties = [
+  { title: "Easy" },
+  { title: "Medium" },
+  { title: "Hard" },
+];
+
+const MATCH_DURATION = 10;
+
 const MatchingPage = () => {
+  const [view, setView] = React.useState(views.selection);
+  const [timerStart, setTimerStart] = React.useState(false);
+  const [remainingTime, setRemainingTime] = React.useState(MATCH_DURATION);
+  const [selected, setSelected] = React.useState(null);
+  const [matchFound, setMatchFound] = React.useState(false);
+
+  // for selection view
+  const handleSubmitMatchRequest = () => {
+    setView(views.loading);
+    setTimerStart(true);
+    submitMatchRequest(selected);
+  };
+
+  const handleQuestionSelect = (index) => {
+    setSelected(index);
+  };
+
+  const submitMatchRequest = (index) => {
+    var questionDifficulty = questionDifficulties[index];
+    console.log("submitting for " + questionDifficulty.title);
+  };
+
+  // for timeout view
+  const handleMatchTimeout = () => {
+    setTimerStart(false);
+    setRemainingTime(MATCH_DURATION);
+    setView(views.timeout);
+  };
+
+  const handleRetryMatch = () => {
+    setView(views.loading);
+    setTimerStart(true);
+    submitMatchRequest(selected);
+  };
+
+  const handleReturnToSelection = () => {
+    setView(views.selection);
+    setSelected(null);
+  };
+
+  // for loading view
+  const handleMatchFound = () => {
+    // modify prop to show session page
+    console.log("handle match found")
+    setTimerStart(false)
+  }
+
+  // timer hook
+  React.useEffect(() => {
+    let interval = null;
+    if (timerStart) {
+      if (remainingTime === 0 && !matchFound) {
+        console.log("handle match timeout");
+        handleMatchTimeout();
+      }
+      else if (remainingTime < MATCH_DURATION && matchFound) {
+        handleMatchFound();
+      }
+      else {
+        interval = setInterval(() => {
+          setRemainingTime((remainingTime) => remainingTime - 1);
+        }, 1000);
+      }
+    } else if (!timerStart && remainingTime !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [timerStart, remainingTime]);
+
+  const renderView = () => {
+    switch (view) {
+      case views.selection:
+        return (
+          <SelectionView
+            handleSubmitMatchRequest={handleSubmitMatchRequest}
+            questionDifficulties={questionDifficulties}
+            handleQuestionSelect={handleQuestionSelect}
+            selected={selected}
+          />
+        );
+      case views.loading:
+        return (
+          <LoadingView
+            remainingTime={remainingTime}
+            matchFound={matchFound}
+          />
+        );
+      case views.timeout:
+        return (
+          <TimeoutView
+            handleRetryMatch={handleRetryMatch}
+            handleReturnToSelection={handleReturnToSelection}
+          />
+        );
+    }
+  };
   return (
     <Layout className="layout">
       <Content style={{ padding: "0 50px", minHeight: "100vh" }}>
-        <Breadcrumb style={{ margin: "16px 0" }}>
-          <Breadcrumb.Item>Matching</Breadcrumb.Item>
-        </Breadcrumb>
+        {renderView()}
       </Content>
     </Layout>
   );
---
+++ frontend/app/src/components/matching/SelectionView.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+import { Layout, Card, Button, Row, Col } from "antd";
+
+const { Content } = Layout;
+
+const gridStyle = {
+  textAlign: "center",
+  cursor: "pointer",
+  width: "100%",
+};
+
+const selectedGridStyle = {
+  textAlign: "center",
+  cursor: "pointer",
+  backgroundColor: "lightblue",
+  width: "100%",
+};
+
+const SelectionView = (props) => {
+  const handleClick = (index) => {
+    props.handleQuestionSelect(index);
+  };
+
+  const handleSubmitMatchRequest = () => {
+    props.handleSubmitMatchRequest();
+  };
+
+  return (
+    <Layout className="layout">
+      <Content style={{ padding: "0 50px", minHeight: "100vh" }}>
+        <Row style={{ height: "50vh" }} justify="center" align="middle">
+          <Col span={18}>
+            <Card title="Select Question Difficulty">
+              {props.questionDifficulties.map((item, index) => {
+                if (index === props.selected) {
+                  return (
+                    <Card.Grid
+                      style={selectedGridStyle}
+                      onClick={() => handleClick(index)}
+                    >
+                      {item.title}
+                    </Card.Grid>
+                  );
+                }
+                return (
+                  <Card.Grid
+                    style={gridStyle}
+                    onClick={() => handleClick(index)}
+                  >
+                    {item.title}
+                  </Card.Grid>
+                );
+              })}
+            </Card>
+          </Col>
+        </Row>
+        <Row style={{ height: "50vh" }} justify="center" align="middle">
+          <Col>
+            <Button
+              onClick={handleSubmitMatchRequest}
+              disabled={props.selected === null}
+            >
+              Find a match!
+            </Button>
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
+  );
+};
+
+export default SelectionView;
