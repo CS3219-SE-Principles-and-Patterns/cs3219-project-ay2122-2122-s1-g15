8137d903854e17a11691fa2c92e0b6c780583184
Jing Ying <e0323887@u.nus.edu>
2021-10-12 02:00:46 +0800

    Configure api gateway

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const gateway = require('express-gateway');
+import '../services/chat/index.js';
+import '../services/editor/index.js';
+import '../services/matching/index.js';
 
 gateway()
   .load(path.join(__dirname, 'config'))
