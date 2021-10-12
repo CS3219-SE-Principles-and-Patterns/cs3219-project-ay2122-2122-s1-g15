56e4c7e48f53e3d30e10b1cc9d706c12936a8fe6
Jing Ying <e0323887@u.nus.edu>
2021-10-12 17:25:35 +0800

    Update config

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,8 +1,5 @@
 const path = require('path');
 const gateway = require('express-gateway');
-import '../services/chat/index.js';
-import '../services/editor/index.js';
-import '../services/matching/index.js';
 
 gateway()
   .load(path.join(__dirname, 'config'))
