8aa7dbbddf985bb5ff5c55430c3154e40251a87a
Rui Zhi <gohruizhi@yahoo.com.sg>
2021-10-12 13:12:52 +0800

    Editor frontend to retrieve connection from service

--- services/editor/index.js
+++ services/editor/index.js
@@ -11,6 +11,15 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+  next();
+});
 const routes = require("./api/routes");
 // Send message for default URL
 app.get("/", (req, res) => res.send("Welcome to the editor service"));
