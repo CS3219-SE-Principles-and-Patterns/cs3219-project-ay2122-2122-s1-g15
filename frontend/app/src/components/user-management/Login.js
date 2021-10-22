5aff822e38baccfee7b6b087775b43ab3a3f7fcc
Jing Ying <e0323887@u.nus.edu>
2021-10-22 13:48:12 +0800

    Add field validations & error modals

--- frontend/app/src/components/user-management/Login.js
+++ frontend/app/src/components/user-management/Login.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Auth from "../../util/Authentication";
 import { useHistory } from "react-router-dom";
-import { Form, Input, Layout, Col, Row, Button, Space } from "antd";
+import { Modal, Form, Input, Layout, Col, Row, Button, Space } from "antd";
+import logo from "./../../images/PeerPrep.png";
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 24 },
+};
 
 export default function Login() {
   const history = useHistory();
   const [form] = Form.useForm();
+  const [disabledLogin, setDisabledLogin] = useState(true);
   const { Content } = Layout;
+
+  const handleFormChange = () => {
+    setDisabledLogin(
+      !form.isFieldsTouched(true) ||
+        form.getFieldsError().filter(({ errors }) => errors.length).length > 0
+    );
+  };
 
   const handleSubmit = (values) => {
     const { email, password } = values;
@@ -15,24 +29,46 @@ export default function Login() {
       .then(() => {
         history.push("/");
       })
-      .catch((error) => console.log("Error while logging in", error));
+      .catch((error) => {
+        console.log("Error while logging in: ", error);
+        showErrorLoggingInModal(error.message);
+      });
+  };
+
+  const showErrorLoggingInModal = (errorMessage) => {
+    Modal.error({
+      title: 'Unable to log into account',
+      content: errorMessage,
+    })
   };
 
   return (
     <Layout id="login" style={{ height: "100vh" }}>
       <Content style={{ padding: "50px" }}>
-        <Col align="center">
-          <Row span={8} align="center">
-            <Form form={form} name="profile" onFinish={handleSubmit}>
-              <p className="formTitle">Login to your PeerPrep account!</p>
+        <Row gutter={16} align="center">
+          <Col span={8} align="center">
+            <Form
+              {...layout}
+              form={form}
+              name="profile"
+              onFieldsChange={handleFormChange}
+              onFinish={handleSubmit}
+            >
+              <img
+                style={{ width: "15%", paddingBottom: 12 }}
+                src={logo}
+                alt="PeerPrep Logo"
+              />
+              <h3 className="formTitle">Login to your PeerPrep account!</h3>
 
               <Form.Item
                 name="email"
-                label="E-mail"
+                style={{ paddingTop: 20 }}
+                label="Email"
                 rules={[
                   {
                     required: true,
-                    message: "Please input your E-mail!",
+                    message: "Please input your email!",
                   },
                 ]}
               >
@@ -51,19 +87,19 @@ export default function Login() {
               >
                 <Input.Password />
               </Form.Item>
-              <Form.Item>
+              <Form.Item style={{ paddingTop: 12 }}>
                 <Space direction="vertical">
-                  <Button type="primary" htmlType="submit">
-                    Login
+                  <Button disabled={disabledLogin} htmlType="submit">
+                    <h4>Login</h4>
                   </Button>
                   <Button type="link" href="/register">
-                    New here? Register here instead instead!
+                    New here? Register here instead!
                   </Button>
                 </Space>
               </Form.Item>
             </Form>
-          </Row>
-        </Col>
+          </Col>
+        </Row>
       </Content>
     </Layout>
   );
