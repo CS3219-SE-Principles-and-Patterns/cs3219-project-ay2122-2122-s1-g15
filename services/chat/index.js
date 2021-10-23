5cb19de1cbd933f89e1a8de733728c3dd2dd1f93
liawsy <liawsy99@gmail.com>
2021-10-23 23:49:21 +0800

    Add tests for chat and handle disconnect events

--- services/chat/index.js
+++ services/chat/index.js
@@ -5,20 +5,23 @@ const DB = "peer-prep";
 const COLLECTION = "socket.io-adapter-events";
 const port = 5000;
 
-const app = require('express')();
-const server = require('http').createServer(app); 
+const app = require("express")();
+const server = require("http").createServer(app);
 
-const mongoClient = new MongoClient("mongodb+srv://chat-admin:fiESyt7PDeQpXtOi@cluster0.tjs9a.mongodb.net/peerprep?retryWrites=true&w=majority", {
-  useUnifiedTopology: true,
+const mongoClient = new MongoClient(
+  "mongodb+srv://chat-admin:fiESyt7PDeQpXtOi@cluster0.tjs9a.mongodb.net/peerprep?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+  }
+);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*", // TODO: Change to our deployed domain later on
+    // methods: ["GET", "POST"],
+    // allowedHeaders: ['my-customer-header']
+  },
 });
-
-const io = require('socket.io')(server, {
-    cors: {
-      origin: "*" // TODO: Change to our deployed domain later on
-      // methods: ["GET", "POST"],
-      // allowedHeaders: ['my-customer-header']
-    }
-  });  
 
 const main = async () => {
   await mongoClient.connect();
@@ -26,7 +29,7 @@ const main = async () => {
   try {
     await mongoClient.db(DB).createCollection(COLLECTION, {
       capped: true,
-      size: 1e6
+      size: 1e6,
     });
   } catch (e) {
     // collection already exists
@@ -35,32 +38,43 @@ const main = async () => {
 
   io.adapter(createAdapter(mongoCollection));
 
-  const rooms = io.of("/").adapter.rooms;
-  const sids = io.of("/").adapter.sids;
-
-  io.on('connection', (socket) => {
-    console.log('user has been connected');
-    socket.on('join room', (sessionId) => {
+  io.on("connection", (socket) => {
+    socket.on("join room", ({ sessionId, username }) => {
       socket.join(sessionId);
-      console.log('socket subscribed to ' + sessionId);
-    })
-    socket.on('chat message', ({message, sender, sessionId}) => {
+      message = `${username} connected!`;
       const payload = {
         message,
-        sender
+        sender: "server",
       };
-      socket.to(sessionId).emit('chat message', payload);
+      socket.to(sessionId).emit("user connected", payload);
+      console.log(`user ${username} has been connected`);
+      console.log(`socket subscribed to ${sessionId}`);
     });
-    socket.on('disconnect', () => {
-      io.emit('user disconnected');
-      console.log('user disconnected');
+    socket.on("chat message", ({ message, sender, sessionId }) => {
+      const payload = {
+        message,
+        sender,
+      };
+      socket.to(sessionId).emit("chat message", payload);
+    });
+    socket.on("initiate disconnect", ({ sessionId, username }) => {
+      message = `${username} disconnected!`;
+      const payload = {
+        message,
+        sender: "server",
+      };
+      socket.to(sessionId).emit("user disconnected", payload);
+    });
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
     });
   });
-  
+
   server.listen(port, () => {
     console.log("Server start on port: " + port);
   });
-
-}
+};
 
 main();
+
+module.exports.main = main;
