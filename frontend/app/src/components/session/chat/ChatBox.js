d0bfa5a5187845ecbcb1877abb8a457aeeed223d
Jing Ying <e0323887@u.nus.edu>
2021-10-31 19:31:48 +0800

    Redeploy frontend

--- frontend/app/src/components/session/chat/ChatBox.js
+++ frontend/app/src/components/session/chat/ChatBox.js
@@ -7,7 +7,12 @@ import "./ChatBox.css";
 import { SessionContext } from "../../../util/SessionProvider";
 
 // TODO: Replace with deployed server endpoint
-const socket = io("https://34.79.116.255/chat/");
+const socket = io("https://34.79.116.255/chat/", {
+  path: "/chat/socket.io/",
+  extraHeaders: {
+    "Access-Control-Allow-Origin": "*",
+  }
+});
 
 const { TextArea } = Input;
 
