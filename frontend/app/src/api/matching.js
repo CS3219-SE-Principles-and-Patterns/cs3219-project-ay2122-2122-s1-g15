474cef8ab20178786a7b977be8b4cd1f1caa3150
Jing Ying <e0323887@u.nus.edu>
2021-11-05 20:07:42 +0800

    Change matching endpoint

--- frontend/app/src/api/matching.js
+++ frontend/app/src/api/matching.js
@@ -1,7 +1,7 @@
 
 import axios from "axios"
 
-const MATCHING_ENDPOINT = "https://34.117.253.13/matching/api"
+const MATCHING_ENDPOINT = "https://peerprep.ninja/matching/socket"
 const API_SUBMIT = process.env.MATCHING_API_SUBMIT || "/match/submit"
 const API_CANCEL = process.env.REACT_APP_MATCHING_API_CANCEL || "/api/match/cancel"
 
