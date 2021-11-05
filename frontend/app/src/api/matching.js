059ed1f6d7a5db3c2ca12525888a2dea1de71f38
Ashleigh <ashleighaathar@gmail.com>
2021-11-06 03:48:43 +0800

    use http polling instead of websocket

--- frontend/app/src/api/matching.js
+++ frontend/app/src/api/matching.js
@@ -3,6 +3,7 @@ import axios from "axios"
 const MATCHING_ENDPOINT = process.env.REACT_APP_MATCHING_ENDPOINT || "http://localhost:4000"
 const API_SUBMIT = process.env.REACT_APP_MATCHING_API_SUBMIT || "/api/match/submit"
 const API_CANCEL = process.env.REACT_APP_MATCHING_API_CANCEL || "/api/match/cancel"
+const API_FIND = process.env.REACT_APP_MATCHING_API_FIND || "/api/match/find"
 
 export const postMatchRequest = async (userObj, difficulty) => {
   var user = {
@@ -31,5 +32,20 @@ export const cancelMatchRequest = async (requestId) => {
     console.log(err)
   })
   
+}
+
+export const findMatch = async (requestId) => {
+  console.log(`axios: ${requestId}`)
+  return axios.put(
+    `${MATCHING_ENDPOINT}${API_FIND}`,
+    {requestId}
+  ).then(res => {
+    console.log(res)
+    return res.data
+  })
+  .catch(err => {
+    console.log(err)
+    return null
+  })
 }
 
