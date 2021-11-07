63dee29965e7dd24f13092232689bd38c68834cf
liawsy <liawsy99@gmail.com>
2021-11-07 17:05:20 +0800

    Fix duplicate messages, add autoscroll for chat

--- frontend/app/src/components/session/chat/ChatBox.js
+++ frontend/app/src/components/session/chat/ChatBox.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import { Card, Input, Button, List } from "antd";
 import { SendOutlined } from "@ant-design/icons";
 import io from "socket.io-client";
@@ -10,6 +10,16 @@ const { uuid } = require("uuidv4");
 
 const { TextArea } = Input;
 
+const socket = io("https://peerprep.ninja", {
+  path: "/chat/socket/socket.io",
+  transports: ["websocket"],
+});
+
+// const socket = io("http://localhost:5000", {
+//   path: "/chat/socket/socket.io",
+//   transports: ["websocket"],
+// });
+
 const ChatBox = (props) => {
   const { username, sessionId, userToken } = props;
   const [message, setMessage] = useState("");
@@ -19,14 +29,19 @@ const ChatBox = (props) => {
   const { initiateDisconnect, setInitiateDisconnect, session, setSession } =
     sessionContext;
 
-  const socket = io("https://peerprep.ninja", {
-    path: "/chat/socket/socket.io",
-    transports: ["websocket"],
-  });
-
   const onChange = (e) => {
     setMessage(e.target.value);
   };
+
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [chat]);
 
   const sendChat = (e) => {
     e.preventDefault();
@@ -54,7 +69,9 @@ const ChatBox = (props) => {
   useEffect(() => {
     socket.on("chat message", (payload) => {
       console.log(payload);
-      setChat((chat) => [...chat, payload]);
+      if (payload.sender !== username) {
+        setChat((chat) => [...chat, payload]);
+      }
     });
     socket.on("user disconnected", (payload) => {
       setChat((chat) => [...chat, payload]);
@@ -82,65 +99,73 @@ const ChatBox = (props) => {
 
   return (
     <>
-      <Card title="Chat" className="card">
-        <div id="chat-container" className="chat-container">
-          {chat.length === 0 ? (
-            <p
-              style={{ color: "#cccccc", textAlign: "center", padding: "12px" }}
-            >
-              <i>This is the beginning of your conversation. Say hi!</i>
-            </p>
-          ) : (
-            <div
-              id="scrollableDiv"
-              className="scrollable"
-              style={{
-                overflow: "auto",
-                padding: "0 16px",
-                height: "600px",
-              }}
-            >
-              <InfiniteScroll
-                dataLength={chat.length}
-                scrollableTarget="scrollableDiv"
+      <div>
+        <Card title="Chat" className="card">
+          <div id="chat-container" className="chat-container">
+            {chat.length === 0 ? (
+              <p
+                style={{
+                  color: "#cccccc",
+                  textAlign: "center",
+                  padding: "12px",
+                  height: "600px",
+                }}
               >
-                <List
-                  className="message-list"
-                  dataSource={chat}
-                  renderItem={(payload, index) => (
-                    <List.Item>
-                      <ChatBubble
-                        key={index}
-                        msg={payload.message}
-                        sender={payload.sender}
-                        username={username}
-                        id={payload.chatId}
-                      />
-                    </List.Item>
-                  )}
-                />
-              </InfiniteScroll>
-            </div>
-          )}
-          <div className="input-container">
-            <TextArea
-              className="chat-input"
-              placeholder="Enter message here!"
-              value={message}
-              onKeyDown={handleKeyDown}
-              autoSize={{ minRows: 1, maxRows: 4 }}
-              onChange={onChange}
-            />
-            <Button
-              type="primary"
-              shape="circle"
-              style={{ marginLeft: "5px" }}
-              icon={<SendOutlined />}
-              onClick={sendChat}
-            />
+                <i>This is the beginning of your conversation. Say hi!</i>
+              </p>
+            ) : (
+              <div
+                id="scrollableDiv"
+                className="scrollable"
+                style={{
+                  overflow: "auto",
+                  padding: "0 16px",
+                  height: "600px",
+                }}
+              >
+                <InfiniteScroll
+                  dataLength={chat.length}
+                  scrollableTarget="scrollableDiv"
+                >
+                  <List
+                    className="message-list"
+                    dataSource={chat}
+                    renderItem={(payload, index) => (
+                      <List.Item>
+                        <ChatBubble
+                          key={index}
+                          msg={payload.message}
+                          sender={payload.sender}
+                          username={username}
+                          id={payload.chatId}
+                        />
+                      </List.Item>
+                    )}
+                  />
+                </InfiniteScroll>
+                <div ref={messagesEndRef} />
+              </div>
+            )}
           </div>
+        </Card>
+        <div className="input-container">
+          <TextArea
+            className="chat-input"
+            placeholder="Enter message here!"
+            value={message}
+            onKeyDown={handleKeyDown}
+            autoSize={{ minRows: 1, maxRows: 4 }}
+            onChange={onChange}
+          />
+          <Button
+            type="primary"
+            shape="circle"
+            style={{ marginLeft: "5px" }}
+            icon={<SendOutlined />}
+            onClick={sendChat}
+          />
         </div>
-      </Card>
+      </div>
     </>
   );
 };
